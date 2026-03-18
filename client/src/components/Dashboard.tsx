b3a3310a0754d3e65ebd4b3679151e49fd48069a
ngirmes <n.girmes1998@gmail.com>
2026-03-17 21:43:44 -0700

    creating variable states for dashboard.tsx

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
+
+type Project = {
+  name: string;
+  tasks: string[];
+};
+
 export default function Dashboard() {
+  const [projects, setProjects] = useState([]);
+  const [currentProject, setCurrentProject] = useState<Project | null>(null);
+  const [tasks, setTasks] = useState([]);
+
   return (
-    <div className="columns-2">
-      <p>Well, let me tell you something, ...</p>
-      <p className="break-before-column">Sure, go ahead, laugh...</p>
-      <p>Maybe we can live without...</p>
-      <p>Look. If you think this is...</p>
-    </div>
+    <>
+      <div className="bg-orange-500 h-screen w-screen relative">
+        <div className="absolute inset-0">
+          <h1 className="">Dashboard</h1>
+          <label htmlFor="project">Add new project:</label>
+          <input
+            id="project"
+            placeholder="Project Name"
+            value={currentProject?.name || ""}
+            type="text"
+            onChange={(e) =>
+              setProjects({ ...currentProject, name: e.target.value })
+            }
+          />
+          <button
+            onClick={() =>
+              setProjects([...projects, { name: projectName, tasks: [] }])
+            }
+          >
+            Add Project
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
---
+++ tasks/tasks.md
@@ -0,0 +1,5 @@
+# Tasks
+
+- add option to add new projects
+  - project will start with an empty task list
+  - user can select the project (maybe from a dropdown?) and add tasks to it in the ui
