cea73e5c033945c500823a2bfcafb3eeb78ee99a
ngirmes <n.girmes1998@gmail.com>
2026-03-19 00:50:04 -0700

    fixed setSession typo

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -6,16 +6,22 @@ import Header from "../components/header";
 
 export default function Dashboard() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [sessionExpired, setSessionExpired] = useState(false);
 
   return (
     <>
       <div className="bg-orange-300 h-screen w-screen flex items-center justify-center">
         <Header />
         <div className="grid grid-cols-3 relative bg-orange-50 p-8 rounded-lg shadow-lg w-3/4 h-3/4 border-2 border-black/70">
-          <MainPanel setIsAuthenticated={setIsAuthenticated} />
+          <MainPanel
+            setIsAuthenticated={setIsAuthenticated}
+            setSessionExpired={setSessionExpired}
+          />
           <AuthPanel
             isAuthenticated={isAuthenticated}
             setIsAuthenticated={setIsAuthenticated}
+            sessionExpired={sessionExpired}
+            setSessionExpired={setSessionExpired}
           />
         </div>
       </div>
